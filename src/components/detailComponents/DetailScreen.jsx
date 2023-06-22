07a15a0a8eee01560d9eeb36bf72e544edac6944
Keldon Lopez <keldon.lopz@gmail.com>
2023-06-21 20:33:44 -0600

    started part 3

--- src/components/detailComponents/DetailScreen.jsx
+++ src/components/detailComponents/DetailScreen.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+import "./DetailScreen.css";
 
-const DetailScreen = () => {  
+const DetailScreen = () => { 
+  
+  const {id} = useParams();
+  const [recipe, setRecipe] =useState('');
+
+  useEffect(() => {
+    axios
+        .get(`https://recipes.devmountain.com/recipes/${id}`)
+        .then((res) => {
+            setRecipe(res.data);
+        });
+}, []);
+    console.log(recipe);
+
   return (
     <section>
-      {/* Welcome to the details page! This page will be reusable. Follow instructions to know what to do here. */}
+      <div 
+      style={{
+        height: "35vh",
+        background: `linear-gradient(
+          190deg,
+          rgba(0, 0, 0, 0.8),
+          rgba(0, 0, 0, 0.8)),
+          url(${recipe.image_url})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        textAlign: "center",
+        color: "white",
+        fontSize: "x-large",
+        padding: "20px",
+      }}
+    >
+      <div className="recipeBanner">
+        <h1>{recipe.recipe_name}</h1>
+      </div>
+    </div>
     </section>
   );
 };
